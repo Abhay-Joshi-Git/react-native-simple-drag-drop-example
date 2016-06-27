861f2f5821228559086726fb517dbe3e63b101cc
abhay-joshi <abhay.joshi@drillinginfo.com>
2016-06-27 17:39:01 +0530

    added list view

--- index.android.js
+++ index.android.js
@@ -18,7 +18,12 @@ class dragDropExample extends Component {
     constructor() {
         super();
 
+        var ds = new ListView.DataSource({
+            rowHasChanged: (r1, r2) => r1 !== r2
+        })
+
         this.state = {
+            dataSource: ds.cloneWithRows(data),
             pan: new Animated.ValueXY({
                 x: 10,
                 y: Window.height - 60
@@ -42,19 +47,61 @@ class dragDropExample extends Component {
             }
         });
 
+        this.renderRow = this._renderRow.bind(this);
+
     }
 
     render() {
         return (
             <View style={styles.container}>
-                <Animated.View
-                    style={[this.state.pan.getLayout(), styles.draggable]}
-                     {...this._panResponder.panHandlers}
-                >
+                {this._getListView()}
+                {this._getDraggableContainerView()}
+            </View>
+        )
+    }
+
+    _getListView() {
+        return (
+            <View style={styles.listViewContainer}>
+                <ListView
+                    dataSource={this.state.dataSource}
+                    renderRow={this.renderRow}
+                />
+            </View>
+        )
+    }
+
+    _getDraggableContainerView() {
+        return (
+            <Animated.View
+                style={[this.state.pan.getLayout(), styles.draggable]}
+                 {...this._panResponder.panHandlers}
+            >
+                <Text>
+                    Selected Item : 2
+                </Text>
+            </Animated.View>
+        )
+    }
+
+    _renderRow(item) {
+        return (
+            <View style={styles.listItemContainer}>
+                <View>
                     <Text>
-                        Selected Item : 2
+                        id: {item.id}
                     </Text>
-                </Animated.View>
+                </View>
+                <View>
+                    <Text>
+                        name: {item.name}
+                    </Text>
+                </View>
+                <View>
+                    <Text>
+                        department: {item.departmentName}
+                    </Text>
+                </View>
             </View>
         )
     }
@@ -65,7 +112,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
         backgroundColor: '#FFFFFF'
     },
     draggable: {
@@ -73,8 +119,17 @@ const styles = StyleSheet.create({
         backgroundColor: '#FAECF7',
         width: 150,
         height: 30
-        // top: Window.height - 60,
-        // left: 10
+    },
+    listItemContainer: {
+        backgroundColor: 'rgb(240, 240, 240)',
+        alignItems: 'flex-start',
+        flex: 1,
+        marginBottom: 2,
+        paddingLeft: 5
+    },
+    listViewContainer: {
+        flex: 1,
+        alignItems: 'stretch'
     }
 });
 
